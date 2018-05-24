372c6bb85ec2247b0996e3eb2e6f9601c97558fc
dkhost <37126556+dkhost@users.noreply.github.com>
2018-05-24 18:55:08 -0300

    Add files via upload

---
+++ Foodtruck.Grafico/TelaListaLanche.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace FoodTruck.Grafico
+{
+    partial class TelaListaLanche
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgLanches = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Valor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btAlterar = new System.Windows.Forms.Button();
+            this.btRemover = new System.Windows.Forms.Button();
+            this.btAdicionar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgLanches)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgLanches
+            // 
+            this.dgLanches.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgLanches.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgLanches.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Nome,
+            this.Valor});
+            this.dgLanches.Location = new System.Drawing.Point(13, 62);
+            this.dgLanches.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgLanches.Name = "dgLanches";
+            this.dgLanches.RowTemplate.Height = 24;
+            this.dgLanches.Size = new System.Drawing.Size(775, 377);
+            this.dgLanches.TabIndex = 11;
+            // 
+            // Id
+            // 
+            this.Id.DataPropertyName = "Id";
+            this.Id.HeaderText = "Codigo";
+            this.Id.Name = "Id";
+            // 
+            // Nome
+            // 
+            this.Nome.DataPropertyName = "Nome";
+            this.Nome.HeaderText = "Nome";
+            this.Nome.Name = "Nome";
+            // 
+            // Valor
+            // 
+            this.Valor.DataPropertyName = "Valor";
+            this.Valor.HeaderText = "Valor";
+            this.Valor.Name = "Valor";
+            // 
+            // btAlterar
+            // 
+            this.btAlterar.Location = new System.Drawing.Point(203, 12);
+            this.btAlterar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btAlterar.Name = "btAlterar";
+            this.btAlterar.Size = new System.Drawing.Size(89, 43);
+            this.btAlterar.TabIndex = 10;
+            this.btAlterar.Text = "Alterar";
+            this.btAlterar.UseVisualStyleBackColor = true;
+            this.btAlterar.Click += new System.EventHandler(this.btAlterar_Click);
+            // 
+            // btRemover
+            // 
+            this.btRemover.Location = new System.Drawing.Point(107, 12);
+            this.btRemover.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btRemover.Name = "btRemover";
+            this.btRemover.Size = new System.Drawing.Size(89, 43);
+            this.btRemover.TabIndex = 9;
+            this.btRemover.Text = "Remover";
+            this.btRemover.UseVisualStyleBackColor = true;
+            this.btRemover.Click += new System.EventHandler(this.btRemover_Click);
+            // 
+            // btAdicionar
+            // 
+            this.btAdicionar.Location = new System.Drawing.Point(12, 12);
+            this.btAdicionar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btAdicionar.Name = "btAdicionar";
+            this.btAdicionar.Size = new System.Drawing.Size(89, 43);
+            this.btAdicionar.TabIndex = 8;
+            this.btAdicionar.Text = "Adicionar";
+            this.btAdicionar.UseVisualStyleBackColor = true;
+            this.btAdicionar.Click += new System.EventHandler(this.btAdicionar_Click);
+            // 
+            // TelaListaLanche
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgLanches);
+            this.Controls.Add(this.btAlterar);
+            this.Controls.Add(this.btRemover);
+            this.Controls.Add(this.btAdicionar);
+            this.Name = "TelaListaLanche";
+            this.Text = "TelaListaLanche";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.TelaListaLanche_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgLanches)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgLanches;
+        private System.Windows.Forms.Button btAlterar;
+        private System.Windows.Forms.Button btRemover;
+        private System.Windows.Forms.Button btAdicionar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Valor;
+    }
+}
---
+++ Foodtruck.Grafico/TelaListaLanche.cs
@@ -0,0 +1,96 @@
+﻿using FoodTruck.Negocio.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FoodTruck.Grafico
+{
+    public partial class TelaListaLanche : Form
+    {
+        public TelaListaLanche()
+        {
+            InitializeComponent();
+        }
+
+        private void AbreTelaInclusaoAlteracao(Lanche lancheSelecionado)
+        {
+            ManterLanche tela = new ManterLanche();
+            tela.MdiParent = this.MdiParent;
+            tela.LancheSelecionado = lancheSelecionado;
+            tela.FormClosed += Tela_FormClosed;
+            tela.Show();
+        }
+
+        private void btAdicionar_Click(object sender, EventArgs e)
+        {
+            AbreTelaInclusaoAlteracao(null);
+        }
+
+        private void Tela_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            CarregarLanches();
+        }
+
+        private void CarregarLanches()
+        {
+            dgLanches.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgLanches.MultiSelect = false;
+            dgLanches.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgLanches.AutoGenerateColumns = false;
+            List<Lanche> lanches = Program.Gerenciador.TodosOsLanches();
+            dgLanches.DataSource = lanches;
+        }
+
+        private void TelaListaLanche_Load(object sender, EventArgs e)
+        {
+            CarregarLanches();
+        }
+
+        private bool VerificarSelecao()
+        {
+            if (dgLanches.SelectedRows.Count<=0)
+            {
+                MessageBox.Show("Selecione uma linha");
+                return false;
+            }
+            return true;
+        }
+
+        private void btRemover_Click(object sender, EventArgs e)
+        {
+            if(VerificarSelecao())
+            {
+                DialogResult resultado = MessageBox.Show("Tem certeza?", "Quer remover?", MessageBoxButtons.OKCancel);
+                if (resultado == DialogResult.OK)
+                {
+                    Lanche lancheSelecionado = (Lanche)dgLanches.SelectedRows[0].DataBoundItem;
+                    var validacao = Program.Gerenciador.RemoverLanche(lancheSelecionado);
+                    if (validacao.Valido)
+                    {
+                        MessageBox.Show("Lanche removido com sucesso!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ocorreu um problema ao remover o lanche.");
+                    }
+                    CarregarLanches();
+                }
+            }
+        }
+
+        private void btAlterar_Click(object sender, EventArgs e)
+        {
+            if (VerificarSelecao())
+            {
+                Lanche lancheSelecionado = (Lanche)dgLanches.SelectedRows[0].DataBoundItem;
+                AbreTelaInclusaoAlteracao(lancheSelecionado);
+            }
+        }
+    }
+}
