46c6f9365ab4ba4f2981d3b7a020cb59b68cacb8
luanrnunes <37030955+luanrnunes@users.noreply.github.com>
2018-05-24 00:28:05 -0300

    Add files via upload

---
+++ Foodtruck.Grafico/ManterBebida.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace FoodTruck.Grafico
+{
+    partial class ManterBebida
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btCancelar = new System.Windows.Forms.Button();
+            this.btSalvar = new System.Windows.Forms.Button();
+            this.tbNome = new System.Windows.Forms.TextBox();
+            this.tbValor = new System.Windows.Forms.TextBox();
+            this.tbTamanho = new System.Windows.Forms.TextBox();
+            this.tbId = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btCancelar
+            // 
+            this.btCancelar.Location = new System.Drawing.Point(508, 207);
+            this.btCancelar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btCancelar.Name = "btCancelar";
+            this.btCancelar.Size = new System.Drawing.Size(131, 53);
+            this.btCancelar.TabIndex = 19;
+            this.btCancelar.Text = "Cancelar";
+            this.btCancelar.UseVisualStyleBackColor = true;
+            this.btCancelar.Click += new System.EventHandler(this.btCancelar_Click);
+            // 
+            // btSalvar
+            // 
+            this.btSalvar.Location = new System.Drawing.Point(658, 207);
+            this.btSalvar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btSalvar.Name = "btSalvar";
+            this.btSalvar.Size = new System.Drawing.Size(131, 53);
+            this.btSalvar.TabIndex = 18;
+            this.btSalvar.Text = "Salvar";
+            this.btSalvar.UseVisualStyleBackColor = true;
+            this.btSalvar.Click += new System.EventHandler(this.btSalvar_Click);
+            // 
+            // tbNome
+            // 
+            this.tbNome.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbNome.Location = new System.Drawing.Point(98, 76);
+            this.tbNome.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbNome.Name = "tbNome";
+            this.tbNome.Size = new System.Drawing.Size(689, 22);
+            this.tbNome.TabIndex = 15;
+            // 
+            // tbValor
+            // 
+            this.tbValor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbValor.Location = new System.Drawing.Point(98, 120);
+            this.tbValor.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbValor.Name = "tbValor";
+            this.tbValor.Size = new System.Drawing.Size(689, 22);
+            this.tbValor.TabIndex = 16;
+            // 
+            // tbTamanho
+            // 
+            this.tbTamanho.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbTamanho.Location = new System.Drawing.Point(98, 162);
+            this.tbTamanho.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbTamanho.Name = "tbTamanho";
+            this.tbTamanho.Size = new System.Drawing.Size(689, 22);
+            this.tbTamanho.TabIndex = 17;
+            // 
+            // tbId
+            // 
+            this.tbId.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbId.Location = new System.Drawing.Point(98, 31);
+            this.tbId.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbId.Name = "tbId";
+            this.tbId.Size = new System.Drawing.Size(689, 22);
+            this.tbId.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Valor:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Tamanho:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Nome:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Código:";
+            // 
+            // ManterBebida
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 285);
+            this.Controls.Add(this.btCancelar);
+            this.Controls.Add(this.btSalvar);
+            this.Controls.Add(this.tbNome);
+            this.Controls.Add(this.tbValor);
+            this.Controls.Add(this.tbTamanho);
+            this.Controls.Add(this.tbId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ManterBebida";
+            this.Text = "ManterBebida";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Shown += new System.EventHandler(this.ManterBebida_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btCancelar;
+        private System.Windows.Forms.Button btSalvar;
+        private System.Windows.Forms.TextBox tbNome;
+        private System.Windows.Forms.TextBox tbValor;
+        private System.Windows.Forms.TextBox tbTamanho;
+        private System.Windows.Forms.TextBox tbId;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Foodtruck.Grafico/ManterBebida.cs
@@ -0,0 +1,77 @@
+﻿using FoodTruck.Negocio;
+using FoodTruck.Negocio.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FoodTruck.Grafico
+{
+    public partial class ManterBebida : Form
+    {
+        public Bebida BebidaSelecionada { get; set; }
+
+        public ManterBebida()
+        {
+            InitializeComponent();
+        }
+
+        private void btSalvar_Click(object sender, EventArgs e)
+        {
+            Bebida bebida = new Bebida();
+
+            bebida.Id = Convert.ToInt64(tbId.Text);
+            bebida.Nome = tbNome.Text;
+            bebida.Valor = Convert.ToDecimal(tbValor.Text);
+            bebida.Tamanho = Convert.ToSingle(tbTamanho.Text);
+
+            Validacao validacao;
+            if (BebidaSelecionada == null)
+            {
+                validacao = Program.Gerenciador.CadastrarBebida(bebida);
+            }
+            else
+            {
+                validacao = Program.Gerenciador.AlterarBebida(bebida);
+            }
+
+            if (!validacao.Valido)
+            {
+                String mensagemValidacao = "";
+                foreach (var chave in validacao.Mensagens.Keys)
+                {
+                    String msg = validacao.Mensagens[chave];
+                    mensagemValidacao += msg;
+                    mensagemValidacao += Environment.NewLine;
+                }
+                MessageBox.Show(mensagemValidacao);
+            }
+            else
+            {
+                MessageBox.Show("Bebida salva com sucesso!");
+                this.Close();
+            }
+        }
+
+        private void btCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ManterBebida_Shown(object sender, EventArgs e)
+        {
+            if (BebidaSelecionada != null)
+            {
+                this.tbId.Text = BebidaSelecionada.Id.ToString();
+                this.tbNome.Text = BebidaSelecionada.Nome;
+                this.tbValor.Text = BebidaSelecionada.Valor.ToString();
+                this.tbTamanho.Text = BebidaSelecionada.Tamanho.ToString();
+            }
+        }
+    }
+}
